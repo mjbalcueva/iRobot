d9c120f496bcb9dfbb8a16a17666362fd5564b46
Mark John Balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-08-22 11:03:02 +0800

    fix grid design. added some javadocs

--- src/program/Grid.java
+++ src/program/Grid.java
@@ -15,18 +15,45 @@ public class Grid {
         table[x][y] = nextCell == 0 ? 0 : 1;
       }
     }
-    table[0][0] = 0;
+    table[0][0] = 0; // always set valid initial position
   }
 
-  // It loops through the position array and prints out each of its element
-  void viewGrid() {
+  /**
+   * It prints the grid with the robot's position marked with an 'R' and the dirt marked with an 'X'
+   * 
+   * @param robot The robot object
+   */
+  void viewGrid(Robot robot) {
     System.out.println("\n--------------------------------------------------\n");
-    for (int[] rows : table) {
-      for (int cols : rows) {
-        System.out.print(cols + " ");
+    for (int x = 0; x < table.length; x++) {
+      for (int y = 0; y < table[x].length; y++) {
+        printCell(robot, x, y);
       }
       System.out.println();
     }
     System.out.println("\n--------------------------------------------------\n");
+  }
+
+  /**
+   * If the robot is at the current cell, print the robot's direction, otherwise print a dot or a
+   * block depending on the value of the cell
+   * 
+   * @param robot The robot object
+   * @param x the x coordinate of the cell
+   * @param y the y coordinate of the robot
+   */
+  private void printCell(Robot robot, int x, int y) {
+    if (x == robot.position[0] && y == robot.position[1]) {
+      if (robot.getDirection().matches("NORTH"))
+        System.out.print("ᐱ ");
+      if (robot.getDirection().matches("WEST"))
+        System.out.print("ᐸ ");
+      if (robot.getDirection().matches("SOUTH"))
+        System.out.print("ᐯ ");
+      if (robot.getDirection().matches("EAST"))
+        System.out.print("ᐳ ");
+    } else {
+      System.out.print((table[x][y] == 0 ? "•" : "■") + " ");
+    }
   }
 }

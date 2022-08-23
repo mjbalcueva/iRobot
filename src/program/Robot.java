1d40b387cb918d54ea749388319adc5d4807bfda
Mark John Balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-08-23 18:44:08 +0800

    refactor to improve code security and efficiency

--- src/program/Robot.java
+++ src/program/Robot.java
@@ -6,7 +6,7 @@ class Robot {
   private int[] position = {0, 0};
 
   Robot(Grid map) {
-    this.map = map.table;
+    this.map = map.getGrid();
   }
 
   /* Checks if the next cell is out of bounds. If so, the iRobot has hit a wall.
@@ -20,35 +20,34 @@ class Robot {
 
     int newX = x += direction.getX();
     int newY = y += direction.getY();
-
     boolean outOfBounds = (x < 0 || y < 0 || x > rows || y > cols);
-    if (outOfBounds) {
-      System.out.println("iRobot has hit a wall. Change direction.");
-      return false;
-    } else if (map[x][y] != 0) {
-      System.out.println("iRobot has hit an obstacle. Change direction.");
+
+    if (outOfBounds || map[x][y] != 0) {
+      String errorMessage = outOfBounds ? "iRobot has hit a wall." : "iRobot has hit an obstacle.";
+      System.out.println(errorMessage + " Change direction.");
       return false;
     } else {
       position = new int[] {newX, newY};
-      System.out.println("iRobot moved to: " + x + ", " + y);
+      System.out.println("iRobot moved to: " + newX + ", " + newY);
       return true;
     }
   }
 
-  /* Creates a for loop that runs n times.
+  /* Prints a new line before the method call.
    * Creates a method call that runs n times.
-   * Prints a new line after the method call.*/
+   * While the iRobot is not out of bounds and not hitting an obstacle, it moves.*/
   public void travel(int n) {
-    for (int i = 0; i < n; i++) {
-      travel();
-    }
     System.out.println();
+    for (int i = 0; i < n; i++) {
+      if (!travel())
+        break;
+    }
   }
 
   /* Checks if the input direction is W, A, S, or D.
    * Changes the direction to the inputted new direction.
    * Prints out an updated direction.*/
-  public void changeDirection(String newDirection) {
+  public void setDirection(String newDirection) {
     if (newDirection.matches("W"))
       direction = Direction.NORTH;
     if (newDirection.matches("A"))
@@ -57,14 +56,18 @@ class Robot {
       direction = Direction.SOUTH;
     if (newDirection.matches("D"))
       direction = Direction.EAST;
-    System.out.println("iRobot has faced " + direction);
+    System.out.println("\niRobot has faced " + direction);
   }
 
-  public String getDirection() {
-    return direction.toString();
+  public Direction getDirection() {
+    return direction;
   }
 
-  public int[] getPosition() {
-    return position;
+  public int posX() {
+    return position[0];
+  }
+
+  public int posY() {
+    return position[1];
   }
 }

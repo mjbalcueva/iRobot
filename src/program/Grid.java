6c900b61df7c6e71cc276c33d25678d39f594aa6
Mark John Balcueva <75126648+mjbalcueva@users.noreply.github.com>
2022-08-23 18:40:10 +0800

    refactor to improve safety and readability

--- src/program/Grid.java
+++ src/program/Grid.java
@@ -4,17 +4,16 @@ import java.util.Random;
 
 public class Grid {
   private Random random = new Random();
-  int[][] table;
+  private int[][] table;
 
   /* Creates a 2D array of integers with the specified number of rows and columns.
    * Fills the array with random numbers values of 0s and 1s.
    * Sets the value of the robot's starting position to 0. */
-  public Grid(int rows, int cols) {
-    table = new int[rows][cols];
+  public Grid(int row, int col) {
+    table = new int[row][col];
     for (int x = 0; x < table.length; x++) {
       for (int y = 0; y < table[x].length; y++) {
-        int nextCell = random.nextInt(3) == 2 ? random.nextInt(2) : 0;
-        table[x][y] = nextCell == 0 ? 0 : 1;
+        table[x][y] = random.nextInt(3) == 2 ? random.nextInt(2) : 0;
       }
     }
     table[0][0] = 0;
@@ -22,33 +21,25 @@ public class Grid {
 
   /* Creates a for loop that iterates through the array.
    * Creates a for loop that iterates through the array's inner array.
+   * Checks if the cell is the robot's position.
+   * Checks if the cell is an obstacle or not.
    * Prints the cell's contents. */
   public void viewGrid(Robot robot) {
     System.out.println("\n--------------------------------------------------\n");
-    for (int x = 0; x < table.length; x++) {
-      for (int y = 0; y < table[x].length; y++) {
-        printCell(robot, x, y);
+    for (int i = 0; i < table.length; i++) {
+      for (int j = 0; j < table[i].length; j++) {
+        if (i == robot.posX() && j == robot.posY())
+          System.out.print(robot.getDirection().getIcon());
+        else
+          System.out.print((table[i][j] == 0 ? "•" : "■") + " ");
       }
       System.out.println();
     }
     System.out.println("\n--------------------------------------------------\n");
+    System.out.println("Press any commands to continue.");
   }
 
-  /* Checks if the cell is the robot's position.
-   * Checks if the cell is an obstacle or not.
-   * Prints the cell's contents. */
-  private void printCell(Robot robot, int x, int y) {
-    if (x == robot.getPosition()[0] && y == robot.getPosition()[1]) {
-      if (robot.getDirection().matches("NORTH"))
-        System.out.print("ᐱ ");
-      if (robot.getDirection().matches("WEST"))
-        System.out.print("ᐸ ");
-      if (robot.getDirection().matches("SOUTH"))
-        System.out.print("ᐯ ");
-      if (robot.getDirection().matches("EAST"))
-        System.out.print("ᐳ ");
-    } else {
-      System.out.print((table[x][y] == 0 ? "•" : "■") + " ");
-    }
+  public int[][] getGrid() {
+    return table;
   }
 }
